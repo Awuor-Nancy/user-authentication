bb799ad837b2a27ba9708b74cdc624aa9c375c1d
Awuor-Nancy <anyangnancy@gmail.com>
2023-01-01 17:15:38 +0300

    Added CRUD methods
    Enable editing, viewing deleting and adding of fields.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 from .views import UserDetailAPI,RegisterUserAPIView
+from django.contrib import admin
+from django.urls import path, include
+from api import views
 
 urlpatterns = [
   path("get-details",UserDetailAPI.as_view()),
   path('register',RegisterUserAPIView.as_view()),
+
+  path('admin', admin.site.urls),
+  path('user', views.user, name = "user"),
+  path('show', views.show, name = "show"),
+  path('edit/', views.edit),
+  path('update/', views.update),
+  path('delete/', views.delete),
 ]
--- api/views.py
+++ api/views.py
@@ -5,6 +5,9 @@ from .serializers import UserSerializer,RegisterSerializer
 from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import generics 
+from django.shortcuts import redirect, render
+from .models import User
+from .forms import UserForm
 
 # Class based view to Get User Details using Token Authentication
 class UserDetailAPI(APIView):
@@ -19,3 +22,39 @@ class UserDetailAPI(APIView):
 class RegisterUserAPIView(generics.CreateAPIView):
   permission_classes = (AllowAny,)
   serializer_class = RegisterSerializer
+
+
+def user(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/show')
+            except:
+                pass
+
+    else: 
+            form = UserForm()
+    return render(request, 'index.html', {'form': form})
+
+def show(request):
+    user1 = User.objects.all()
+    return render(request, 'show.html', {'employees': user1})
+
+def edit(request, id):
+    user2 = User.objects.get(id = id) 
+    return render(request, 'edit.html', {'employee': user2})
+
+def update(request, id):
+    user3 = User.objects.get(id = id)
+    form = UserForm(request.POST, instance = user3)
+    if form.is_valid():
+        form.save()
+        return redirect('/show')
+    return render(request, 'edit.html', {'emp':user3})  
+    
+def delete(request, id):
+    user4 = User.objects.get(id = id)
+    user4.delete()
+    return redirect('/show')
